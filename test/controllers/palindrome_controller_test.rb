5541853c8d86cdf22158ad0bcf20b807957b9e94
magnus_test <magnus_test@test.com>
2020-10-11 02:39:51 +0300

    adding tests with success

--- test/controllers/palindrome_controller_test.rb
+++ test/controllers/palindrome_controller_test.rb
@@ -11,4 +11,26 @@ class PalindromeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get count=6 with value=32" do
+    get palindrome_view_url, params: {val: 32}
+    assert_equal assigns[:count], 6
+  end
+
+  test "should get count=11 with value=250" do
+    get palindrome_view_url, params: {val: 250}
+    assert_equal assigns[:count], 11
+  end
+
+  test "should get count=0 with value<=0" do
+    value = -10
+    get palindrome_view_url, params: {val: value}
+    assert_equal assigns[:count], 0
+  end
+
+  test "should get count=0 with string-value" do
+    value = "text"
+    get palindrome_view_url, params: {val: value}
+    assert_equal assigns[:count], 0
+  end
+
 end
